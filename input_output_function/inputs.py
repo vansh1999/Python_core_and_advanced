7c4f7169cb5fe5aad5c579dcaa20cc55f36ca271
vansh1999 <vansh.bhardwaj1999@gmail.com>
2019-12-20 03:32:00 +0530

    input output

---
+++ input_output_function/inputs.py
@@ -0,0 +1,11 @@
+num  = int(input("enetr number "))
+
+# print(num)  # error
+
+# read multiple inputs use split()
+
+
+a,b,c = input().split()
+
+print("a is {} b is {} and c is {}".format(a,b,c))
+
---
+++ input_output_function/printtype.py
@@ -0,0 +1,14 @@
+a , b = 10 ,30
+
+print(a,b)
+print(a,b , sep=",")
+print(a,b,sep="+++")
+
+name = "vansh"
+marks = 90
+
+print("name is " , name , "and marks is " , marks)  #1
+print("name is %s and marks is %d" %(name ,marks) )  #2
+print("name is {} and marks is {}".format(name , marks))  #3
+
+
