3d2d004d274c23c43cd7405ae60d15dff9e65093
vansh1999 <vansh.bhardwaj1999@gmail.com>
2019-12-22 04:46:14 +0530

    add module

---
+++ modules/mymath.py
@@ -0,0 +1,11 @@
+
+def adding(a,b):
+    return a+b
+
+def multi(a,b):
+    return a*b
+
+def addthree(a,b,c):
+    return a+b+c
+    
+            
---
+++ modules/vanshbha.py
@@ -0,0 +1,28 @@
+
+import mymath
+
+result1 = mymath.adding(5,6)
+
+print(result1)
+
+result2 = mymath.multi(2,7)
+result3 = mymath.addthree(1,3,1)
+
+print(result2)
+print(result3)
+
+# 1
+# also use can import 
+# import mymath as myma 
+# giving mymath a alice name myma and use it myma
+# result = myma.adding(3,3)
+# reult = myma.multi(2,3)
+
+
+# 2 use 
+# from mymath import * (* --- all)
+
+# from mymath import *
+
+# print(adding(1,10))
+
